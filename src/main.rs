3ff783d2581fa0bfd0961872f3824ed41172dc05
AtsukiYokota <atsuki.yokota@gmail.com>
2020-01-22 18:08:17 +0900

    Add main routine for lexer

--- src/main.rs
+++ src/main.rs
@@ -182,10 +182,31 @@ fn skip_spaces(input: &[u8], pos: usize) -> Result<((), usize), LexError> {
     Ok(((), pos))
 }
 
-
+fn prompt(s: &str) -> io::Result<()> {
+    use std::io::{stdout, Write};
+    let stdout = stdout();
+    let mut stdout = stdout.lock();
+    stdout.write(s.as_bytes())?;
+    stdout.flush()
+}
 
 fn main() {
-    println!("Hello, world!");
+    use std::io::{stdin, BufRead, BufReader};
+
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let stdin = BufReader::new(stdin);
+    let mut lines = stdin.lines();
+
+    loop {
+        prompt("> ").unwrap();
+        if let Some(Ok(line)) = lines.next() {
+            let token = lex(&line);
+            println!("{:?}", token);
+        } else {
+            break;
+        }
+    }
 }
 
 #[cfg(test)]
